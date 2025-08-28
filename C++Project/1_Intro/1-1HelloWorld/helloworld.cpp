1ab6c77962a558179e7182a5d0528320ea7fffe2
larry <fpnf97@outlook.com>
2025-08-28 23:36:45 +0800

    又做了一个测试

--- C++Project/1_Intro/1-1HelloWorld/helloworld.cpp
+++ C++Project/1_Intro/1-1HelloWorld/helloworld.cpp
@@ -2,7 +2,7 @@
 using namespace std; // This line allows us to use standard names from the std name
 
 int main() {
-    cout << "Hello, World!" << endl; // This line prints "Hello, World!" to the console
+    cout << "Hello, world again!" << endl; // This line prints "Hello, World!" to the console
     system("pause"); // This line is used to pause the console window in some environments
     cin.get(); // Wait for user input before closing the console window
     return 0; // Return 0 indicates successful completion of the program
