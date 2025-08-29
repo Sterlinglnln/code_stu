9d99b00292a0552de3d64e6d79b49594aea669fc
larry <fpnf97@outlook.com>
2025-08-29 20:31:07 +0800

    2025.08.29_daily_python_stu

---
+++ PyProject/1_Intro/02guess_number.py
@@ -0,0 +1,16 @@
+secret_number = 42
+
+number_of_guess = 1
+
+# 玩家猜数字
+user_guess = int(input("Please enter your guess number: "))
+
+while user_guess != secret_number and number_of_guess < 5:
+    print("Wrong guess, try again!")
+    user_guess = int(input("Please enter your guess number: "))
+    number_of_guess += 1
+
+if user_guess == secret_number:
+    print("Congratulations! You guessed it right.")
+else:
+    print("Sorry, you've used all your attempts. The secret number was:", secret_number)
