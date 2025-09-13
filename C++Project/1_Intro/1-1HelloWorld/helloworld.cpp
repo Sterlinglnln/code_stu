79662de4eb44df5e147b9f1db79dcfa2448a45f4
sterlinglnln <fpnf97@outlook.com>
2025-09-13 15:20:33 +0800

    1

--- C++Project/1_Intro/1-1HelloWorld/helloworld.cpp
+++ C++Project/1_Intro/1-1HelloWorld/helloworld.cpp
@@ -1,9 +1,6 @@
-#include <iostream> // This line includes the iostream library
-using namespace std; // This line allows us to use standard names from the std name
+#include <iostream>
 
 int main() {
-    cout << "Hello, world again!" << endl; // This line prints "Hello, World!" to the console
-    system("pause"); // This line is used to pause the console window in some environments
-    cin.get(); // Wait for user input before closing the console window
-    return 0; // Return 0 indicates successful completion of the program
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
