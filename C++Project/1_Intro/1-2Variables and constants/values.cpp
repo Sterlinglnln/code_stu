1251b30d2ef4dd56acc4894a1e6c78484b474a6d
sterlinglnln <fpnf97@outlook.com>
2025-09-13 15:22:52 +0800

    1

--- C++Project/1_Intro/1-2Variables and constants/values.cpp
+++ C++Project/1_Intro/1-2Variables and constants/values.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-#define week 7 // The value of week is defined as 7 using a macro
+#define week 7
 
 int main() {
     cout << "The value of week is: " << week << endl;
 
-    const int month = 12; // The value of month is defined as 12 using a constant variable
+    const int month = 12;
     cout << "The value of month is: " << month << endl;
 
     return 0;
