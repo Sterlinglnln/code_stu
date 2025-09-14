554a1ef48baac32284fbeb76f8d865b1562e1154
sterlinglnln <fpnf97@outlook.com>
2025-09-14 23:35:48 +0800

    换一种写法

--- Leetcode/1_TwoNumsPlus.cpp
+++ Leetcode/1_TwoNumsPlus.cpp
@@ -4,18 +4,57 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> result;
-    for (int i = 0; i < nums.size(); ++i) {
-        for (int j = i + 1; j < nums.size(); ++j) {
-            if (nums[i] + nums[j] == target) {
-                result.push_back(i);
-                result.push_back(j);
-                return result;
-            }
-        }
+vector<int> twoSum(const vector<int>& nums, int target) {
+    // 输入验证
+    if (nums.empty()) {
+        throw invalid_argument("输入数组不能为空");
     }
-    return result;
+    
+    unordered_map<int, int> numMap; // 存储数值及其索引
+    
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        // 使用迭代器一次查找操作，避免二次哈希查找
+        auto it = numMap.find(complement);
+        if (it != numMap.end()) {
+            return {it->second, i};
+        }
+        // 仅在未找到时插入，减少不必要的操作
+        numMap.emplace(nums[i], i);
+    }
+    
+    // 如果没有找到符合条件的两个数，抛出异常
+    throw invalid_argument("没有找到符合条件的两个数");
+}
+
+int main() {
+    try {
+        // 测试用例1：基础功能
+        vector<int> nums1 = {2, 7, 11, 15};
+        int target1 = 22;
+        vector<int> result1 = twoSum(nums1, target1);
+        cout << "测试用例1: 下标为 " << result1[0] << " 和 " << result1[1] << endl;
+        
+        // 测试用例2：包含重复元素
+        vector<int> nums2 = {3, 3};
+        int target2 = 6;
+        vector<int> result2 = twoSum(nums2, target2);
+        cout << "测试用例2: 下标为 " << result2[0] << " 和 " << result2[1] << endl;
+        
+        // 测试用例3：负数情况
+        vector<int> nums3 = {-1, 5, 3};
+        int target3 = 2;
+        vector<int> result3 = twoSum(nums3, target3);
+        cout << "测试用例3: 下标为 " << result3[0] << " 和 " << result3[1] << endl;
+        
+    } catch (const invalid_argument& e) {
+        cerr << "错误: " << e.what() << endl;
+        return 1;
+    }
+    
+    return 0;
 }
