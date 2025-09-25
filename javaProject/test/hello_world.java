b3b8e1ef6e1508dbe68c7548e1d0e2a9ef868f4b
Lin Junbao <fpnf97@outlook.com>
2025-09-25 15:14:29 +0800

    my first java

---
+++ javaProject/test/hello_world.java
@@ -0,0 +1,6 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
