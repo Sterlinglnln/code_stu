0cc7a0bf044a5407048eded757e49996d96e0bc2
larry <fpnf97@outlook.com>
2025-10-03 02:56:13 +0800

    test

--- C++Project/1_Intro/1-1HelloWorld/helloworld.cpp
+++ C++Project/1_Intro/1-1HelloWorld/helloworld.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
+class MessageFromCpp {
+private:
+    string message;
+
+public:
+    MessageFromCpp(string& msg) : message(msg) {
+        cout << "Message from C++: " << message << endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    string myMessage, cppMessage;
+    cout << "Enter my message: ";
+    getline(cin, myMessage);
+
+    cout << "Enter C++ message: ";
+    getline(cin, cppMessage);
+    MessageFromCpp msgObj(cppMessage);
+
     return 0;
 }
